07083a100f49f55b8b9af2ce78e5c6db2267b9f1
Raja Prabhu <yogarani2014@gmail.com>
2019-08-12 14:56:33 +0530

    First commit

---
+++ src/main/java/com/asminds/MavenDemo/Controller/MavenDemoController.java
@@ -0,0 +1,23 @@
+package com.asminds.MavenDemo.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MavenDemoController {
+	@RequestMapping("/") 
+	public String index()
+	{
+		System.out.println("I am in home");
+		return "index";
+		
+	}
+	@RequestMapping("/hello") 
+	public String home()
+	{
+		System.out.println("I am in home");
+		return "hello";
+		
+	}
+	
+}
